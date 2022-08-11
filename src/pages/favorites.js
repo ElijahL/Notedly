6fb1be74006a558d76f53e12c14446d74599474e
Elijah <elia.zmushko@gmail.com>
2022-08-11 11:28:21 +0300

    init commit

---
+++ src/pages/favorites.js
@@ -0,0 +1,31 @@
+import React, { useEffect } from 'react';
+import { useQuery, gql } from '@apollo/client';
+
+import NoteFeed from '../components/NoteFeed';
+import { GET_MY_FAVORITES } from '../gql/query';
+
+const Favorites = () => {
+    useEffect(() => {
+        document.title = 'Favorites - Notedly';
+    });
+
+    const { data, loading, error } = useQuery(GET_MY_FAVORITES);
+
+    if (loading) {
+        return 'Loading...';
+    }
+
+    if (error) {
+        return `Error! ${error.message}`;
+    }
+
+    if (data.me.favorites.length == 0) {
+        return <p>No favorites yet</p>
+    }
+
+    return (
+        <NoteFeed notes={data.me.favorites} />
+    );
+};
+
+export default Favorites;
